ce672c4f553c8cedfb98fca560d02942ca0d42d7
xiangyu.xie <xiangyu.xie@psi.ch>
2025-04-25 14:37:27 +0200

    Add MC processing codes

---
+++ McUsefulFunctions.py
@@ -0,0 +1,128 @@
+import os
+import numpy as np
+from ROOT import *
+from multiprocessing import Pool
+from array import array
+from scipy.stats import gennorm
+import UsefulFuncs
+
+EnergyPerPair = 3.62 # eV
+FanoFactor = 0.13
+
+
+_cfg = {} ### global config
+_noiseEneFrame = None ### global noiseEneFrame
+h1_ClusterEnergy, h1_PixelEnergy, h1_CenterPixelEnergy, h2_Qc, h1_ChargeCollectionEfficiency = None, None, None, None, None
+
+def init(cfg):
+    global _cfg
+    _cfg = cfg
+
+def processOneDepth(idxDepth):
+    energy = _cfg['energy'] ### eV
+    selectionRange = _cfg['selectionRange']
+    Roi = _cfg['Roi'] ### [x1, x2, y1, y2], for noise sampling from measured noise map
+
+    pdf = _cfg['pdfList'][idxDepth]
+    ggdParams = _cfg['ggdParList'][idxDepth]
+    nTotalIncident = _cfg['nTotalIncident']
+    pixelSize = _cfg['pixelSize'] ### um
+    clusterWidth = _cfg['clusterWidth'] ### um
+
+    _frameWidth = 9
+    h1_ClusterEnergy, h1_PixelEnergy, h1_CenterPixelEnergy, h2_Qc, h1_ChargeCollectionEfficiency = UsefulFuncs.bookHistograms(energy/1000, suffix = f'_{idxDepth}', isMC= True)
+    
+    for _ in range(round(nTotalIncident * pdf)):
+        nExpectedPair = round(energy / EnergyPerPair)
+        nPair = np.random.normal(nExpectedPair, np.sqrt(energy * FanoFactor / EnergyPerPair))
+        nPair = round(nPair)
+        ### sample the incident position
+        x_center = np.random.uniform(pixelSize*_frameWidth//2, pixelSize*_frameWidth//2 + pixelSize)
+        y_center = np.random.uniform(pixelSize*_frameWidth//2, pixelSize*_frameWidth//2 + pixelSize)
+        ### sample pair positions
+        x_pairs = gennorm.rvs(beta = ggdParams[0], loc = x_center, scale = ggdParams[1], size = nPair)
+        y_pairs = gennorm.rvs(beta = ggdParams[0], loc = y_center, scale = ggdParams[1], size = nPair)
+        x_pairs = x_pairs//pixelSize
+        y_pairs = y_pairs//pixelSize
+        ### put the pairs into the frame
+        _carrierArray = np.zeros((_frameWidth, _frameWidth))
+        np.add.at(_carrierArray, (y_pairs.astype(np.int32), x_pairs.astype(np.int32)), 1)
+        _energyArray = _carrierArray * EnergyPerPair
+
+        ### spread noise
+        x_pedetal = np.random.randint(Roi[0], Roi[1] - _frameWidth)
+        y_pedetal = np.random.randint(Roi[2], Roi[3] - _frameWidth)
+        _pedestalNoiseFrame = _cfg['noiseEneFrame'][y_pedetal:y_pedetal + _frameWidth, x_pedetal:x_pedetal + _frameWidth]
+
+        shotNoiseFactor = _cfg['shotNoiseFactor']
+        _shotNoiseFrame = np.sqrt(_energyArray * shotNoiseFactor)
+        _calibrationNoiseFrame = np.ones_like(_energyArray) * _cfg['calibrationNoise']
+        _noiseFrame = np.random.normal(0, np.sqrt(_pedestalNoiseFrame**2 + _shotNoiseFrame**2 + _calibrationNoiseFrame**2), size = _energyArray.shape)
+        _energyArray += _noiseFrame
+
+        ### truncate to desired cluster width
+        ### odd width: highest pixel in the center
+        ### even width: highest pixel in the center 2x2 is selected to maximize the cluster energy
+        highestPixel = (np.argmax(_energyArray)//_frameWidth, np.argmax(_energyArray)%_frameWidth)
+        if clusterWidth%2 == 1:
+            pixelArray = _energyArray[highestPixel[0]-clusterWidth//2:highestPixel[0]+clusterWidth//2+1, highestPixel[1]-clusterWidth//2:highestPixel[1]+clusterWidth//2+1]
+            carrierArray = _carrierArray[highestPixel[0]-clusterWidth//2:highestPixel[0]+clusterWidth//2+1, highestPixel[1]-clusterWidth//2:highestPixel[1]+clusterWidth//2+1]
+        else:
+            clusterEnergy = 0 
+            for i in range(2):
+                for j in range(2):
+                    _pixelArray = _energyArray[highestPixel[0]-clusterWidth//2+i:highestPixel[0]+clusterWidth//2+i, highestPixel[1]-clusterWidth//2+j:highestPixel[1]+clusterWidth//2+j]
+                    _carrierArray = _carrierArray[highestPixel[0]-clusterWidth//2+i:highestPixel[0]+clusterWidth//2+i, highestPixel[1]-clusterWidth//2+j:highestPixel[1]+clusterWidth//2+j]
+                    if np.sum(_pixelArray) > clusterEnergy:
+                        clusterEnergy = np.sum(_pixelArray)
+                        pixelArray = _pixelArray
+                        carrierArray = _carrierArray
+        ### fill histograms
+        clusterEnergy = np.sum(pixelArray)
+        h1_ClusterEnergy.Fill(clusterEnergy/1000)
+        if energy - selectionRange*1000 < clusterEnergy < energy + selectionRange*1000:
+            h1_PixelEnergy.FillN(len(pixelArray.flatten()), array('d', pixelArray.flatten()/1000), array('d', np.ones(len(pixelArray.flatten()))))
+            x_energyCenter = (np.average(np.arange(clusterWidth), weights = pixelArray.sum(axis=0)) + 0.5)%1
+            y_energyCenter = (np.average(np.arange(clusterWidth), weights = pixelArray.sum(axis=1)) + 0.5)%1
+            h2_Qc.Fill(x_energyCenter, y_energyCenter)
+            h1_ChargeCollectionEfficiency.Fill(np.sum(carrierArray) / nPair)
+            h1_CenterPixelEnergy.Fill(_energyArray[highestPixel[0], highestPixel[1]]/1000)
+    return h1_ClusterEnergy.Clone(), h1_PixelEnergy.Clone(), h1_CenterPixelEnergy.Clone(), h2_Qc.Clone(), h1_ChargeCollectionEfficiency.Clone()
+
+def process():
+    with Pool(processes = _cfg['nThread']) as pool:
+        results = pool.map(processOneDepth, range(len(_cfg['pdfList'])))
+
+    global h1_ClusterEnergy, h1_PixelEnergy, h1_CenterPixelEnergy, h2_Qc, h1_ChargeCollectionEfficiency
+    h1_ClusterEnergy, h1_PixelEnergy, h1_CenterPixelEnergy, h2_Qc, h1_ChargeCollectionEfficiency = UsefulFuncs.bookHistograms(_cfg['energy']/1000, suffix = '_MC', isMC= True)
+    for _h1_ClusterEnergy, _h1_PixelEnergy, _h1_CenterPixelEnergy, _h2_Qc, _h1_ChargeCollectionEfficiency in results:
+        h1_ClusterEnergy.Add(_h1_ClusterEnergy)
+        h1_PixelEnergy.Add(_h1_PixelEnergy)
+        h1_CenterPixelEnergy.Add(_h1_CenterPixelEnergy)
+        h2_Qc.Add(_h2_Qc)
+        h1_ChargeCollectionEfficiency.Add(_h1_ChargeCollectionEfficiency)
+
+    c = TCanvas("c", "c", 1600, 1600)
+    c.Divide(2, 2)
+    pad1 = c.cd(1)
+    h1_ClusterEnergy.SetTitle(';Cluster Energy [keV];Counts')
+    h1_ClusterEnergy.Draw()
+
+    pad2 = c.cd(2)
+    h1_PixelEnergy.SetTitle(';Pixel Energy [keV];Counts')
+    h1_PixelEnergy.Draw()
+    pad2.SetLogy()
+
+    pad3 = c.cd(3)
+    h1_CenterPixelEnergy.SetTitle(';Center Pixel Energy [keV];Counts')
+    h1_CenterPixelEnergy.Draw()
+    pad3.SetLogy()
+
+    pad4 = c.cd(4)
+    h1_ChargeCollectionEfficiency.SetTitle(';Charge Collection Efficiency;Counts')
+    h1_ChargeCollectionEfficiency.Draw()
+    clusterWidth = _cfg['clusterWidth']
+    print(f'{clusterWidth}x{clusterWidth} cluster, CCE = {h1_ChargeCollectionEfficiency.GetMean()}')
+    pad4.SetLogy()
+
+    return c.Clone()
